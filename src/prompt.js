08afd01bf445ee080935832f03035f40b2b02517
Matt Brennan <matt@153.io>
2018-06-06 13:18:06 +0100

    remove a console.log

--- src/prompt.js
+++ src/prompt.js
@@ -6,7 +6,6 @@ const prefix = chalk.green.bold.italic('  ?');
 const runPrompt = (choice, argv = {}) => [].concat(choice).reduce(
 	async (argvPromise, spec) => {
 		const argv = await argvPromise;
-		console.log(argv);
 
 		const answer = spec.name in argv
 			? {[spec.name]: argv[spec.name]} // skip prompt if it's already been given (e.g. by CLI args)
