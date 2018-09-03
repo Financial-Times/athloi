5617f6f6d582a5c00846598cb520b81358089792
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-09-03 16:47:53 +0100

    Log concurrency level  🐿 v2.10.0

--- src/run-parallel.js
+++ src/run-parallel.js
@@ -1,7 +1,10 @@
+const logger = require('./logger');
 const Sema = require('async-sema');
 
 module.exports = (tasks = [], concurrency = 1) => {
 	const sema = new Sema(concurrency);
+
+	logger.info(`Executing up to ${concurrency} tasks at a time`);
 
 	return Promise.all(
 		tasks.map((task) => {
