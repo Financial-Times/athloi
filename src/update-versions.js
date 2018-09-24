65f4af14302ff85c5d7eb4e9aa2b835573f763e1
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-09-24 14:09:28 +0100

    Only bump dependency versions if they are using a local file path  🐿 v2.10.0

--- src/update-versions.js
+++ src/update-versions.js
@@ -1,24 +1,27 @@
 const clone = (src) => JSON.parse(JSON.stringify(src));
 
+const targetProperties = [
+	'dependencies',
+	'devDependencies',
+	'peerDependencies',
+	'optionalDependencies'
+];
+
 module.exports = (manifest, number, localDependencies = []) => {
 	const pkg = clone(manifest);
 
 	pkg.version = number;
 
-	for (const packageName of localDependencies) {
-		const range = `^${number}`;
+	const range = `^${number}`;
 
-		if (pkg.dependencies && pkg.dependencies[packageName]) {
-			pkg.dependencies[packageName] = range;
-		}
-		if (pkg.devDependencies && pkg.devDependencies[packageName]) {
-			pkg.devDependencies[packageName] = range;
-		}
-		if (pkg.peerDependencies && pkg.peerDependencies[packageName]) {
-			pkg.peerDependencies[packageName] = range;
-		}
-		if (pkg.optionalDependencies && pkg.optionalDependencies[packageName]) {
-			pkg.optionalDependencies[packageName] = range;
+	for (const packageName of localDependencies) {
+		for (const targetProperty of targetProperties) {
+			const version = pkg[targetProperty] && pkg[targetProperty][packageName];
+
+			// Only update dependencies using relative package paths
+			if (version && /^file:/.test(version)) {
+				pkg[targetProperty][packageName] = range;
+			}
 		}
 	}
 
