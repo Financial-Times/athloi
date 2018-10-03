f6cf4440ee91215dd2d2723d561eb9e26a0dd0c0
Matt Hinchliffe <matt@maketea.co.uk>
2018-10-03 10:23:08 +0100

    Add module to fetch latest package versions from the npm registry
     🐿 v2.10.0

---
+++ src/get-latest.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+
+const fetchJSON = async (url) => {
+	const response = await fetch(url);
+
+	if (response.ok) {
+		return response.json();
+	}
+
+	if (response.status === 404) {
+		return null;
+	}
+
+	return Promise.reject(`The request to "${url}" returned a ${response.status}`);
+};
+
+module.exports = async (packages = []) => {
+	const public = packages.filter((pkg) => pkg.private === false);
+	const requests = public.map((pkg) => fetchJSON(`https://registry.npmjs.org/${pkg.name}`));
+	const results = await Promise.all(requests);
+
+	// map all of the successful registry requests
+	const available = results.filter(Boolean).reduce((map, result) => {
+		return map.set(result.name, result['dist-tags'].latest);
+	}, new Map());
+
+	return packages.reduce((map, { name }) => {
+		return map.set(name, available.has(name) ? available.get(name) : '0.0.0');
+	}, new Map());
+};
