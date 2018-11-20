8511e004fb7aa4a03b913e743cf074890733f144
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-11-20 17:12:38 +0000

    Extend filter packages helper to support object and array comparison  🐿 v2.10.0

--- src/filter-packages.js
+++ src/filter-packages.js
@@ -1,9 +1,21 @@
+const test = (a, b) => {
+	if (Array.isArray(a) && typeof b === 'string') {
+		return a.includes(b);
+	}
+
+	if (typeof a === 'object' && typeof b === 'string') {
+		return a.hasOwnProperty(b);
+	}
+
+	return a === b;
+};
+
 module.exports = (filter, packages = []) => (
 	filter
 		? packages.filter(({ manifest }) => {
 			if (filter.includes(':')) {
 				const [key, value] = filter.split(':');
-				return manifest[key] === JSON.parse(value);
+				return test(manifest[key], JSON.parse(value));
 			} else {
 				// By default filter on the package name
 				return manifest.name.split('/').slice(-1)[0] === filter;
