97c617ee34bd0d54b91141f33250d44830a93dd2
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-02-14 14:27:43 +0000

    Ignore glob matches which are not directories containing a package.json file  🐿 v2.10.0

--- src/load-manifest.js
+++ src/load-manifest.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
 const path = require('path');
 
 module.exports = (packagePath) => {
 	const manifestPath = path.resolve(packagePath, 'package.json');
-	return require(manifestPath);
+
+	if (fs.existsSync(manifestPath)) {
+		return require(manifestPath);
+	}
 };
