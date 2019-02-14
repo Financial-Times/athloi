9c8168f78c868301dd5f56d0e2798487a3672008
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-02-14 14:49:55 +0000

    Create an explicit error when directories are matched without a package.json file  🐿 v2.10.0

--- src/load-manifest.js
+++ src/load-manifest.js
@@ -2,9 +2,15 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (packagePath) => {
-	const manifestPath = path.resolve(packagePath, 'package.json');
+	const stats = fs.statSync(packagePath);
 
-	if (fs.existsSync(manifestPath)) {
-		return require(manifestPath);
+	if (stats.isDirectory()) {
+		const manifestPath = path.resolve(packagePath, 'package.json');
+
+		if (fs.existsSync(manifestPath)) {
+			return require(manifestPath);
+		} else {
+			throw Error(`Folder found without package.json file: ${packagePath}`)
+		}
 	}
 };
