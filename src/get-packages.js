703f383110c07716ee77d647cd67d1a9525992cb
Matt Hinchliffe <i-like-robots@users.noreply.github.com>
2019-02-20 14:37:33 +0000

    Reject with human readable error message when packages config is invalid, fixes #32

--- src/get-packages.js
+++ src/get-packages.js
@@ -2,7 +2,12 @@ const { promisify } = require('util');
 const glob = promisify(require('glob'));
 
 module.exports = (patterns = []) => {
+	if (!Array.isArray(patterns)) {
+		return Promise.reject(Error('Packages or workspaces configuration must be an array.'));
+	}
+
 	const opts = { realpath: true };
+
 	// <https://en.wikipedia.org/wiki/Glob_(programming)>
 	return glob(patterns.length > 1 ? `{${patterns.join(',')}}` : patterns[0], opts);
 };
