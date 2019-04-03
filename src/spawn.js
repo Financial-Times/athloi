c6d1895c79a5175a0ce203de66f02a5231b05273
Matt Hinchliffe <i-like-robots@users.noreply.github.com>
2019-04-03 13:33:22 +0100

    Close stdin stream when spawning child processes
    This is a micro-optimisation which can speed up subprocess execution time by 100-200ms.

--- src/spawn.js
+++ src/spawn.js
@@ -8,6 +8,9 @@ module.exports = (cmd, args = [], opts = {}) => {
 	return new Promise((resolve, reject) => {
 		const child = spawn(cmd, args, { env: process.env, ...opts });
 
+		// Closing the input stream is a micro-optimisation and can speed up a task by 100-200ms
+		child.stdin.end();
+
 		byline(child.stdout).on('data', (line) => logger.message(cleanLine(line)));
 
 		// stderr is not always used for error logging so rely on non-zero exit code
