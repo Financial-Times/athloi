10e3fee5ec8eb46ef4a4a351b5eb8a3e2f936499
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-04-04 13:02:31 +0100

    Add more joy to Athloi task completion  🐿 v2.12.3

--- src/cli-task.js
+++ src/cli-task.js
@@ -47,7 +47,7 @@ module.exports = (task) => {
 
 			timer.stop();
 
-			logger.success(`Tasks complete, took ${timer.duration}s`);
+			logger.debug(`✨Tasks complete, took ${timer.duration}s`);
 		} catch (error) {
 			const message = error instanceof Error ? error.message : error;
 			const exitCode = Number.isInteger(error.code) ? error.code : 1;
