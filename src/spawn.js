cbb1fd7738fe934b97086781bbb48df17e444371
Matt Hinchliffe <matt@maketea.co.uk>
2019-04-10 09:32:15 +0100

    Maintain log order by buffering stdout and stderr together  🐿 v2.12.0

--- src/spawn.js
+++ src/spawn.js
@@ -6,23 +6,19 @@ const cleanLine = (line) => line.toString('utf8').replace(/^>\s+/, '');
 
 module.exports = (cmd, args = [], opts = {}) => {
 	return new Promise((resolve, reject) => {
-		const stdout = [];
-		const stderr = [];
-
+		const logs = [];
 		const child = spawn(cmd, args, { env: process.env, ...opts });
 
 		// Closing the input stream is a micro-optimisation and can speed up a task by 100-200ms
 		child.stdin.end();
 
-		byline(child.stdout).on('data', (line) => stdout.push(cleanLine(line)));
+		byline(child.stdout).on('data', (line) => logs.push(cleanLine(line)));
 
-		byline(child.stderr).on('data', (line) => stderr.push(cleanLine(line)));
+		byline(child.stderr).on('data', (line) => logs.push(cleanLine(line)));
 
 		child.on('error', reject);
 
 		child.on('exit', (code) => {
-			const logs = [...stdout, ...stderr];
-
 			if (code > 0) {
 				const error = Error(`Exited with code ${code}`);
 				error.code = code;
