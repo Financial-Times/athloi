d2d63ce65eeeec335c5f499b4cffee70391c6daf
geoff.thorpe <geoff.thorpe"ft.com>
2023-04-20 10:16:37 +0100

    temporary debug logs

--- src/run-parallel.js
+++ src/run-parallel.js
@@ -12,30 +12,31 @@ module.exports = (tasks = [], concurrency = 1, preserveOrder = false) => {
 
 	return Promise.all(
 		tasks.map(async ({ pkg, apply }) => {
-			console.log(`@@@@@@@@@@@@@@@ START ${pkg.name} @@@@@@@@@@@@@@@@@@`);
+			console.log(`@@@@@@@@@@ START ${pkg.name} @@@@@@@@@@@@@@@@@@`);
+			console.log(apply);
 			queue.add(pkg.name);
 
-			console.log('@@@@@@@@@@ before acquire @@@@@@@@@@');
+			console.log(`@@@@@@@@@@ before acquire ${pkg.name} @@@@@@@@@@`);
 			await semaphore.acquire();
-			console.log('@@@@@@@@@@ after acquire @@@@@@@@@@');
+			console.log(`@@@@@@@@@@ after acquire ${pkg.name} @@@@@@@@@@`);
 
 			// wait for any dependencies still in the queue to finish
 			if (preserveOrder) {
 				console.log(
-					'@@@@@@@@@@@@@@@@@@ waiting for dependency @@@@@@@@@@@@@@@@@',
+					`@@@@@@@@@@@ waiting for dependency ${pkg.name} @@@@@@@@@`,
 				);
 				console.log(pkg.allDependencies);
 				await queue.waitBehind(pkg.allDependencies);
 			}
 
-			console.log('@@@@@@@@@@ before apply @@@@@@@@@@');
+			console.log(`@@@@@@@@@@ before apply ${pkg.name} @@@@@@@@@@`);
 			await apply();
-			console.log('@@@@@@@@@@ after apply @@@@@@@@@@');
+			console.log(`@@@@@@@@@@ after apply ${pkg.name} @@@@@@@@@@`);
 
 			queue.delete(pkg.name);
 
 			semaphore.release();
-			console.log(`@@@@@@@@@@@@@@@ END ${pkg.name} @@@@@@@@@@@@@@@@@@`);
+			console.log(`@@@@@@@@@ END ${pkg.name} @@@@@@@@@@`);
 		}),
 	);
 };
